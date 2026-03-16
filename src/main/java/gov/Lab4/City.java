c8352b96a83ead3cff73bc0fa8c12ddbcd50bcb7
Melrumiris <96199034+Melrumiris@users.noreply.github.com>
2026-03-16 13:58:40 +0200

    Lab4 Generator

--- src/main/java/gov/Lab4/City.java
+++ src/main/java/gov/Lab4/City.java
@@ -2,13 +2,12 @@ package gov.Lab4;
 
 import org.jgrapht.Graph;
 import org.jgrapht.GraphPath;
-import org.jgrapht.alg.interfaces.SpanningTreeAlgorithm;
-import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
+import org.jgrapht.alg.connectivity.BiconnectivityInspector;
 import org.jgrapht.alg.spanning.PrimMinimumSpanningTree;
 import org.jgrapht.alg.tour.ChristofidesThreeHalvesApproxMetricTSP;
+import org.jgrapht.alg.util.Triple;
 import org.jgrapht.graph.AsWeightedGraph;
 import org.jgrapht.graph.SimpleGraph;
-import org.jgrapht.graph.SimpleWeightedGraph;
 import org.jgrapht.graph.builder.GraphBuilder;
 
 import java.awt.*;
@@ -17,7 +16,7 @@ import java.util.List;
 import java.util.function.Supplier;
 
 public class City {
-    Graph<Intersection, Street> graph;
+    final Graph<Intersection, Street> graph;
 
     public City() {
         graph = new SimpleGraph<>(IntersectionSupplier.getInstance(), StreetSupplier.getInstance(), true);
@@ -66,29 +65,91 @@ public class City {
         return graph;
     }
 
+    public boolean isThreeJoined(Street s){
+        boolean S = graph.degreeOf(graph.getEdgeSource(s)) >= 3;
+        boolean T = graph.degreeOf(graph.getEdgeTarget(s)) >= 3;
+        return S || T;
+    }
+
     public List<Iterator<Street>> computeSpanningTrees(int count) {
         List<Iterator<Street>> iteratorList = new ArrayList<>();
         SimpleGraph<Intersection, Street> copy = new GraphBuilder<>(new SimpleGraph<>(IntersectionSupplier.getInstance(), StreetSupplier.getInstance(), true))
                 .addGraph(graph)
                 .build();
         for (int i = 0; i < count; i++) {
-            PrimMinimumSpanningTree<Intersection,Street> algorithm = new PrimMinimumSpanningTree<>(new AsWeightedGraph<Intersection,Street>(graph, Street::getLength, false,false));
+            PrimMinimumSpanningTree<Intersection,Street> algorithm = new PrimMinimumSpanningTree<>(new AsWeightedGraph<>(graph, Street::getLength, false, false));
             iteratorList.add(algorithm.getSpanningTree().iterator());
             if (i == count - 1){
-                algorithm.getSpanningTree().getEdges().stream().min(Comparator.comparingDouble(Street::getLength)).ifPresent((Street s) -> graph.removeEdge(s));
+                var bridges = new BiconnectivityInspector<>(copy).getBridges();
+                algorithm.getSpanningTree().getEdges().stream().filter(bridges::contains).min(Comparator.comparingDouble(Street::getLength)).ifPresent((Street s) -> graph.removeEdge(s));
             }
         }
-
         return iteratorList;
     }
 
-    public GraphPath<Intersection, Street> computeTour(Intersection from, Intersection to) {
-        return new ChristofidesThreeHalvesApproxMetricTSP<Intersection,Street>().getTour(new AsWeightedGraph<>(graph, Street::getLength, false,false));
+    public GraphPath<Intersection, Street> computeTour() {
+        return new ChristofidesThreeHalvesApproxMetricTSP<Intersection, Street>().getTour(new AsWeightedGraph<>(graph, Street::getLength, false, false));
     }
 
-    static public City generateCity(){
+    static public City generateCity(int minIntersections, int maxIntersections){
+        if (minIntersections > maxIntersections) throw new IllegalArgumentException("max can't be smaller than min");
         City city = new City();
+        Supplier<Intersection> interGen = IntersectionSupplier.getInstance();
+        ArrayList<Intersection> intersections = new ArrayList<>();
+        Random rand = new Random();
+        for (int i = 0; i < rand.nextInt(maxIntersections - minIntersections) + minIntersections; i++) {
+            Intersection obj = interGen.get();
+            city.addIntersection(obj);
+            intersections.add(obj);
+        }
+        for (int i = 0; i < rand.nextInt(intersections.size()*(intersections.size()-1)/2); i++) {
+            Intersection from = intersections.get(rand.nextInt(intersections.size()));
+            Intersection to = intersections.get(rand.nextInt(intersections.size()));
+            city.addStreet(from, to);
+        }
 
+        HashSet<Triple<Street, Street, Street>> triangles = city.getTriangleCycles();
+        boolean valid = false;
+        while (!valid) {
+            valid = true;
+            for (Triple<Street, Street, Street> t : triangles) {
+                Street s1 = t.getFirst();
+                Street s2 = t.getSecond();
+                Street s3 = t.getThird();
+                double l1 = s1.getLength();
+                double l2 = s2.getLength();
+                double l3 = s3.getLength();
+                if (l1 + l2 <= l3) {
+                    s3.setLength((l1 + l2) - 1);
+                    valid = false;
+                } else if (l1 + l3 <= l2) {
+                    s2.setLength((l1 + l3) - 1);
+                    valid = false;
+                } else if (l2 + l3 <= l1) {
+                    s1.setLength((l2 + l3) - 1);
+                    valid = false;
+                }
+            }
+        }
         return city;
+    }
+
+    HashSet<Triple<Street,Street,Street>> getTriangleCycles() {
+        HashSet<Triple<Street,Street,Street>> result = new HashSet<>();
+        for (Street s1 : graph.edgeSet()) {
+            Intersection u = graph.getEdgeSource(s1);
+            Intersection v = graph.getEdgeTarget(s1);
+            for (Street s2 : graph.edgesOf(u)) {
+                if (s2 == s1) continue;
+                Intersection w = graph.getEdgeSource(s2).equals(u) ? graph.getEdgeTarget(s2) : graph.getEdgeSource(s2);
+                Street s3 = graph.getEdge(v, w);
+                if (s3 != null) {
+                    List<Street> tri = Arrays.asList(s1, s2, s3);
+                    tri.sort(Comparator.comparingInt(System::identityHashCode));
+                    result.add(new Triple<>(tri.get(0), tri.get(1), tri.get(2)));
+                }
+            }
+        }
+        return result;
     }
 }
--- src/main/java/gov/Lab4/Homework/Main.java
+++ src/main/java/gov/Lab4/Homework/Main.java
@@ -4,9 +4,6 @@ import gov.Lab4.City;
 import gov.Lab4.Intersection;
 import gov.Lab4.IntersectionSupplier;
 import gov.Lab4.Street;
-import org.jgrapht.alg.util.Pair;
-import org.jgrapht.graph.SimpleGraph;
-import org.jgrapht.graph.builder.GraphBuilder;
 
 import java.util.ArrayList;
 import java.util.Random;
@@ -23,18 +20,11 @@ public class Main {
             city.addIntersection(obj);
             intersections.add(obj);
         }
-        {
-            Intersection i0 = intersections.get(rand.nextInt(intersections.size()));
-            Intersection i1 = intersections.get(rand.nextInt(intersections.size()));
-            Intersection i2 = intersections.get(rand.nextInt(intersections.size()));
-            city.addStreet(i0, i1).addStreet(i1, i2).addStreet(i2, intersections.get(rand.nextInt(intersections.size())));
-
-        }
         for (int i = 0; i < 100; i++) {
             Intersection from = intersections.get(rand.nextInt(intersections.size()));
             Intersection to = intersections.get(rand.nextInt(intersections.size()));
             city.addStreet(from, to);
         }
-        city.getStreets().stream().filter((Street s)-> s.getLength() > 250).forEach(System.out::println);
+        city.getStreets().stream().filter((Street s)-> s.getLength() > 250).filter(city::isThreeJoined).forEach(System.out::println);
     }
 }
